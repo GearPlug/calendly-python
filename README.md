e2c0555248444e12cc7fdcd89efae996679afe7b
juanrios15 <juankrios15@gmail.com>
2022-12-22 17:01:28 -0500

    added: client init access token, get current user, create webhook

--- README.md
+++ README.md
@@ -5,3 +5,31 @@ calendly-python is an API wrapper for Calendly, written in Python
 ## Installing
 ```
 pip install calendly-python
+```
+## Usage
+```
+from calendly.client import Client
+client = Client('access_token')
+
+user_uri = client.user_uri
+user_uuid = client.user_uuid
+organization_uri = client.organization_uri
+organization_uuid = client.organization_uuid
+```
+#### Current User
+```
+current_user = client.get_current_user()
+```
+### Webhooks
+#### Create webhook
+```
+webhook = client.create_webhook(self, url, events, organization_uri, user_uri, scope)
+# events: must be a list of valid events (check calendly API)
+# scope: two options: "user" or "organization"
+```
+#### List webhooks
+ ```
+webhooks = client.list_webhooks(self, scope, organization_uri, user_uri=None)
+# scope: two options: "user" or "organization"
+# Note: must send user_uri if scope = "user"
+```
--- calendly/client.py
+++ calendly/client.py
@@ -4,7 +4,81 @@ from calendly.exceptions import UnauthorizedError, WrongFormatInputError, Contac
 
 
 class Client(object):
-    pass
+    url = "https://api.calendly.com/"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
 
-    def __init__(self):
-        pass
+    def __init__(self, access_token):
+        self.headers.update(Authorization=f"Bearer {access_token}")
+        current_user = self.get_current_user()["resource"]
+        self.user_uri = current_user["uri"]
+        self.user_uuid = current_user["uri"].split("/")[-1]
+        self.organization_uri = current_user["current_organization"]
+        self.organization_uuid = current_user["current_organization"].split("/")[-1]
+
+    def get_current_user(self):
+        return self.get('users/me')
+
+    def create_webhook(self, url, events, organization_uri, user_uri, scope):
+        body = {
+            "url": url,
+            "events": events,
+            "organization": organization_uri,
+            "user": user_uri,
+            "scope": scope
+        }
+        return self.post('webhook_subscriptions', data=body)
+
+    def list_webhooks(self, scope, organization_uri, user_uri=None):
+        params = {"scope": scope, "organization": organization_uri}
+        if scope == "user":
+            params.update(user=user_uri)
+        return self.get('webhook_subscriptions',params=params)
+
+    def get(self, endpoint, params=None):
+        response = self.request('GET', endpoint, params=params)
+        return self.parse(response)
+
+    def post(self, endpoint, params=None, data=None, headers=None, json=True):
+        response = self.request('POST', endpoint, params=params, data=data, headers=headers, json=json)
+        return self.parse(response)
+
+    def delete(self, endpoint, params=None):
+        response = self.request('DELETE', endpoint, params=params)
+        return self.parse(response)
+
+    def request(self, method, endpoint, params=None, data=None, headers=None, json=True):
+        _headers = self.headers
+        if headers:
+            _headers.update(headers)
+        kwargs = {}
+        if json:
+            kwargs['json'] = data
+        else:
+            kwargs['data'] = data
+        return requests.request(method, self.url + endpoint, params=params, headers=_headers, **kwargs)
+
+    def parse(self, response):
+        status_code = response.status_code
+        if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:
+            try:
+                r = response.json()
+            except ValueError:
+                r = response.text
+        else:
+            r = response.text
+        if status_code == 200:
+            return r
+        if status_code == 204:
+            return None
+        if status_code == 400:
+            raise WrongFormatInputError(r)
+        if status_code == 401:
+            raise UnauthorizedError(r)
+        if status_code == 406:
+            raise ContactsLimitExceededError(r)
+        if status_code == 500:
+            raise Exception
+        return r
